5fdbc0dcfe75d38c20e161238089d0bc894a3559
irfankhanktk <irfan.khan93093@gmail.com>
2022-12-11 21:29:03 +0500

    changes

--- src/pages/paymentData.js
+++ src/pages/paymentData.js
@@ -8,7 +8,7 @@ import TableRow from "@mui/material/TableRow";
 import profile from "../assets/profile.svg";
 import Paper from "@mui/material/Paper";
 import { Box } from "@mui/system";
-import { Grid, Stack } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
@@ -16,6 +16,27 @@ function createData(name, calories, fat, carbs, protein) {
 
 var arr = [
   {
+    id: 1,
+    profile: profile,
+    email: "1@gmail.com",
+    payment: "$125.56",
+  },
+  {
+    id: 1,
+    profile: profile,
+    email: "1@gmail.com",
+    payment: "$125.56",
+  }, {
+    id: 1,
+    profile: profile,
+    email: "1@gmail.com",
+    payment: "$125.56",
+  }, {
+    id: 1,
+    profile: profile,
+    email: "1@gmail.com",
+    payment: "$125.56",
+  }, {
     id: 1,
     profile: profile,
     email: "1@gmail.com",
@@ -74,30 +95,36 @@ export default function PaymentData() {
         </Box>
       </Stack>
 
-      <TableContainer component={Paper} sx={{ backgroundColor: "#F5F5F5" }}>
-        <Table sx={{ minWidth: 100 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="left">Usernames</TableCell>
-              <TableCell align="right">Total payment</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {arr.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell align="left" scope="row">
-                  <img src={row.profile} />
-                  {row.email}
-                </TableCell>
-                <TableCell align="right">{row.payment}</TableCell>
+      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+        <TableContainer sx={{ maxHeight: 600 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">Usernames</TableCell>
+                <TableCell align="right">Total payment</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {arr.map((row) => (
+                <TableRow
+                  key={row.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell align="center" scope="row">
+                    <Grid container alignItems={'center'}>
+                      <img src={row.profile} />
+                      <Typography ml={'10px'}>
+                        {`${row.email}`}
+                      </Typography>
+                    </Grid>
+                  </TableCell>
+                  <TableCell align="right">{row.payment}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     </Box>
   );
 }
