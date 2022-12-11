6b3244ae1bf93724fb1539fadc71f43b967b58da
Ashiq-jutt <jtt00088@gmail.com>
2022-12-11 22:52:58 +0500

    change name page added

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,8 @@
-export { default as group } from './group.svg'
-export { default as earnings } from './earnings.svg'
-export { default as payments } from './payments.svg'
-export { default as reportedContent } from './reported-content.svg'
-export { default as settings } from './settings.svg'
-export { default as dollar } from './dollar.svg'
-export { default as arrowUp } from './arrow-up.svg'
+export { default as group } from "./group.svg";
+export { default as earnings } from "./earnings.svg";
+export { default as payments } from "./payments.svg";
+export { default as reportedContent } from "./reported-content.svg";
+export { default as settings } from "./settings.svg";
+export { default as dollar } from "./dollar.svg";
+export { default as arrowUp } from "./arrow-up.svg";
+export { default as left } from "./left.svg";
